e02bfa7aa6b18f46c58e9a90495ac2a7cfd5630d
warboomsplayz <70512932+warboomsplayz@users.noreply.github.com>
2020-11-22 16:21:27 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,21 @@
+class Box extends BaseClass {
+  constructor(x, y){
+    super(x,y,60,60);
+    this.image = loadImage("wood1.png");
+    this.Visiblity = 255
+  }
+ display(){
+   //console.log(this.body.speed);
+   if(this.body.speed < 10){
+    super.display();
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
+   }
+  }
+};
